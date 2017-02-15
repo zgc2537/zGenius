6d0d6270867d53a2fe56135b24872d5486475f2d
zgc2537 <zgc_2537@hotmail.com>
2017-02-15 17:54:20 +0800

    Update lazy_scroll.js
    1.加载条可自定义附属容器
    2.修复无初始数据时出现无更多数据问题，以及无法加载数据问题
    3.所有控件均提供Id

--- lazyscroll/js/lazy_scroll.js
+++ lazyscroll/js/lazy_scroll.js
@@ -4,56 +4,60 @@
 !function (a, b, c) {
     var lazyScroll = function (options) {
         this.opts = $.extend({
-                outerid : '', //内容容器的外部容器id，一般高度小于等于屏幕高度的容器
-                innerid : '', //内容容器id，装载内容的直接容器
-                hElement : undefined, //动态加载单个内容项，与slhfe互斥，两个参数选其一进行复制，
-                                      //如果两个参数都有，则以此参数的clientHeight为准
-                ehratio : 2, //与hElement配合使用，从内容容器底部开始向上hElement.clientHeight * ehratio的高度处开始加载新的内容
-                slhfe : 100, //从内容容器底部开始向上的高度，从此高度就开始加载新的内容
-                haveMore : false, //是否还有更多的内容
+                outerCId : '', //内容容器的外部容器id，一般高度小于等于屏幕高度的容器
+                innerCId : '', //内容容器id，装载内容的直接容器
+                loadingAId: undefined,//加载条所在的容器Id,默认放在inner容器底部
+                hElement : undefined, //动态加载代表行高的单个内容项Id，与slhfe互斥，两个参数选其一进行复制，
+                                      //如果两个参数都有，则以此参数为准
+                hERatio : 2, //hElement，从内容容器底部开始向上hElement.clientHeight * ehratio的高度处开始加载新的内容
+                slhfe : 400, //从内容容器底部开始向上的高度，从此高度就开始加载新的内容
+                haveMoreData : false, //是否还有更多的内容
+                loadingCb : null, //用户自定义加载数据动作，由控件自动调用
                 haveInitData : true, //是否有初始数据
-                loadingcb : null, //用户自定义加载数据动作，由控件自动调用
-                loadingtext : '正在加载...',
-                nomoretext : '没有更多了'
+                loadingText : '正在加载...',
+                noMoreText : '没有更多了'
             }, options || {}),
-        this.elementH = this.opts.hElement ? this.opts.hElement.clientHeight : 0,
         this.loading = false,
         this.tipObj = null,
         this.tipLabelObj = null,
         this.tipHtmlStr = '<div id="ls_sts_bar" class = "split_loading"><span class="ls-loading">\
-        <div class="ls-loading-container container1"><div class="circle1"></div><div class="circle2"></div>\
-        <div class="circle3"></div><div class="circle4"></div></div><div class="ls-loading-container container2">\
-        <div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div>\
-        </div><div class="ls-loading-container container3"><div class="circle1"></div><div class="circle2"></div>\
-        <div class="circle3"></div><div class="circle4"></div></div></span><span class="sts_label">'
-        + this.opts.loadingtext + '</span></div>',
-        this.outerVistibleHeight = $('#' + this.opts.outerid).get(0).clientHeight,
+			<div class="ls-loading-container container1"><div class="ls-loading-container container2">\
+			<div class="circle1"></div><div class="circle2"></div><div class="circle3"></div>\
+			<div class="circle4"></div></div><div class="ls-loading-container container3">\
+			<div class="circle1"></div><div class="circle2"></div><div class="circle3"></div>\
+			<div class="circle4"></div></div></span><span class="sts_label">'
+                + this.opts.loadingText + '</span></div>',
+        this.outerVistibleHeight = $('#' + this.opts.outerCId).get(0).clientHeight,
         this.initGetData = true;
+        this._calElementHeight();
         this._init();
     };
     lazyScroll.prototype = {
         _init : function () {
             if(!this.opts.haveInitData) {
                 this.loading = true;
-                this.opts.loadingcb();
+                this.opts.loadingCb();
             }
             this._insertFooterUI();
-            if (this.opts.haveMore) {
-                this._bindScrollEvent();
-            } else {
+            if (this.opts.haveInitData && !this.opts.haveMoreData) {
                 this._changeToNoMoreUi();
                 this._unbindScrollEvent();
+            } else {
+                this._bindScrollEvent();
             }
+        },
+        _calElementHeight : function() {
+			this.elementH = $("#" + this.opts.hElement).clientHeight;
         },
         _scrollHandler : function () {
             var scrollT = $(window).scrollTop(),
-            loadingHeightFromBottom = this.lazyScroll.elementH ? this.lazyScroll.elementH * this.lazyScroll.opts.ehratio :
-                this.lazyScroll.opts.slhfe;
+            loadingHeightFromBottom = this.lazyScroll.elementH ? this.lazyScroll.elementH * this.lazyScroll.opts.hERatio :
+				this.lazyScroll.opts.slhfe;
 
-            if ($('#' + this.lazyScroll.opts.innerid).get(0).clientHeight - this.lazyScroll.outerVistibleHeight -
-                loadingHeightFromBottom < scrollT && !this.lazyScroll.loading) {
+            if ($('#' + this.lazyScroll.opts.innerCId).get(0).clientHeight - this.lazyScroll.outerVistibleHeight -
+					loadingHeightFromBottom < scrollT && !this.lazyScroll.loading) {
                 this.lazyScroll.loading = true;
-                this.lazyScroll.opts.loadingcb();
+                this.lazyScroll.opts.loadingCb();
             }
         },
         /** 内容加载完成后，由外部调用此方法
@@ -61,7 +65,7 @@
          **/
         loadEnd : function (more) {
             if(!this.opts.haveInitData && this.initGetData) {
-                this.elementH = this.opts.hElement ? this.opts.hElement.clientHeight : 0;
+                this._calElementHeight();
                 this.initGetData = false;
                 this.tipObj.remove();
                 this._insertFooterUI();
@@ -71,16 +75,24 @@
                 this._unbindScrollEvent();
             }
             this.loading = false;
+
+        },
+        removeFooterUI: function () {
+            this.tipObj.remove();
         },
         _insertFooterUI : function () {
-            $('#' + this.opts.innerid).append(this.tipHtmlStr);
+            if(this.opts.loadingAId) {
+                $('#' + this.opts.loadingAId).append(this.tipHtmlStr);
+            } else {
+                $('#' + this.opts.innerCId).append(this.tipHtmlStr);
+            }
             this.tipObj = $('#ls_sts_bar');
             this.tipLabelObj = $('.sts_label');
         },
         _changeToNoMoreUi : function () {
             this.tipObj.removeClass("split_loading");
             this.tipObj.addClass("nomore");
-            this.tipLabelObj.text(this.opts.nomoretext);
+            this.tipLabelObj.text(this.opts.noMoreText);
         },
         _bindScrollEvent : function () {
             $(window).scroll(this._scrollHandler);
